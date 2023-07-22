9f093fa66a57d76604e4c609808314dd667f8c46
Eric Roth <ecrsoccerdude@gmail.com>
2023-07-22 16:41:43 -0500

    Basic CORS fix

--- flask/server.py
+++ flask/server.py
@@ -1,15 +1,32 @@
 from flask import Flask, jsonify
-from flask_cors import cross_origin
 
 app = Flask(__name__)
 
-@app.route("/students/", methods=['GET'])
-@cross_origin(origin='*')
+@app.after_request
+def add_cors_headers(response):
+    """
+    Append CORS headers to every request.
+
+    NOTE: Unsafe for deplpoyment. Should only be used for development purposes.
+    """
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+
+    return response
+
+@app.route('/')
+def root():
+    return "This is root"
+
+@app.route('/test')
+def test():
+    return "testing"
+
+@app.route("/students", methods=["GET"])
 def students():
     """
-    Returns currently checked-in students
-
-    NOTE: This should only be used for testing right now
+    Returns currently checked-in students. Currently just a dummy.
     """
     students = [
         {
@@ -30,4 +47,4 @@ def students():
     return jsonify(students)
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=5000)
