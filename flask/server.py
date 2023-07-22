ad273bd0b3e8add8921666a381c38c167ce1ae28
Eric Roth <ecrsoccerdude@gmail.com>
2023-07-22 15:15:39 -0500

    Alow cross-origin

--- flask/server.py
+++ flask/server.py
@@ -1,28 +1,33 @@
-import flask
+from flask import Flask, jsonify
+from flask_cors import cross_origin
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/students/')
+@app.route("/students/", methods=['GET'])
+@cross_origin(origin='*')
 def students():
     """
     Returns currently checked-in students
 
     NOTE: This should only be used for testing right now
     """
-    return {
-        'students': [
-            {
-                'firstName': 'Eric',
-                'lastName': 'Roth',
-                'lab': 'AYC',
-                'date': '1690054337',
-                'notes': 'None'
-            }, {
-                'firstName': 'Tyler',
-                'lastName': 'Cushing',
-                'lab': 'AYC',
-                'date': '1690054389',
-                'notes': 'None'
-            }
-        ]
-    }
+    students = [
+        {
+            'firstName': 'Eric',
+            'lastName': 'Roth',
+            'lab': 'AYC',
+            'date': '1690054337',
+            'notes': 'None'
+        }, {
+            'firstName': 'Tyler',
+            'lastName': 'Cushing',
+            'lab': 'AYC',
+            'date': '1690054389',
+            'notes': 'None'
+        }
+    ]
+
+    return jsonify(students)
+
+if __name__=='__main__':
+    app.run(debug=True)
